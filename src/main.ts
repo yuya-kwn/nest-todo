db0461664f67f09303186ddc7e3b590e1d5d3a73
yuya-kwn <k.yuya413@icloud.com>
2023-07-07 19:06:50 +0900

    feat: add enablecors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const prisma = app.get(PrismaService);
   prisma.enableShutdownHooks(app);
+  app.enableCors();
   await app.listen(8080);
 }
 bootstrap();
