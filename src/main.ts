f1b8caaee6479bfaff0042ef4de7ec3588f2067f
yuya-kwn <k.yuya413@icloud.com>
2023-07-07 11:16:03 +0900

    fix: close DB

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const prisma = app.get(PrismaService);
+  prisma.enableShutdownHooks(app);
   await app.listen(8080);
 }
 bootstrap();
